f10c3dc30b41dd37090e3515f096f209eea42aef
melany.estevez <mel.estevez99@gmail.com>
2023-10-22 21:46:44 -0400

    feat: base Rest API

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# TxtFileTangoAPI
-REST API that uses Minio as storage to upload and download files
+# About this repo
+
+This repository contains a Python REST API project that is designed to handle the upload and management of TXT files. The API allows users to  upload and retrieve text files in a simple and straightforward manner.
+
+## Requirements
+
+- Python 3.7 or higher
+- Fast Api
+- minio docker container
+
+## Setup 
+
+```bash
+AWS_BUCKET_NAME=
+ACCESS_KEY=
+SECRET_KEY=
+HOST_BUCKET=
+SIGNATURE_VERSION=
+RESOURCE_TYPE=
+PORT=
+```
+
+## Installation
+
+```bash
+python -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+python app.py
+```
+
+## Usage
+
+```bash
+http://0.0.0.0:8000/docs
+``` 
+
+## Resources
+
+[boto3](https://stackoverflow.com/questions/42809096/difference-in-boto3-between-resource-client-and-session)
+
+[upload](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/s3/bucket/upload_fileobj.html)
+
---
+++ main.py
@@ -0,0 +1,39 @@
+from dotenv import load_dotenv
+load_dotenv()
+import os
+import boto3
+from botocore.client import Config
+from fastapi import FastAPI, UploadFile, File, HTTPException
+from botocore.exceptions import NoCredentialsError,ClientError
+
+s3=boto3.resource(os.getenv("RESOURCE_TYPE"), 
+    endpoint_url=os.getenv("HOST_BUCKET"),
+    aws_access_key_id=os.getenv("ACCESS_KEY"),
+    aws_secret_access_key=os.getenv("SECRET_KEY"),
+    aws_session_token=None,
+    config=boto3.session.Config(signature_version=os.getenv("SIGNATURE_VERSION")),
+    verify=False
+)
+
+bucket = s3.Bucket(os.getenv("AWS_BUCKET_NAME"))
+app = FastAPI()
+@app.post("/upload/")
+async def upload_file(file: UploadFile):
+    try:
+        with file.file as f:
+            bucket.upload_fileobj(f,file.filename)
+        return {"message": "File uploaded successfully"}
+    except NoCredentialsError:
+        raise HTTPException(status_code=500, detail="AWS credentials not available")
+@app.get("/download/{file_name}")
+async def download_file(file_name: str):
+    try:
+        bucket.download_file(file_name, f'{file_name}.txt')
+    except ClientError as e:
+        if e.response['Error']['Code'] == "404":
+            print("The object does not exist.")
+        else:
+            raise
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=int(os.getenv("PORT")))
